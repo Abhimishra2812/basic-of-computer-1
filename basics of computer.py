d7a13a5f28e4946978622d853fbc4079f27bbed1
Abhimishra2812 <101655063+Abhimishra2812@users.noreply.github.com>
2022-03-15 21:10:28 +0530

    Add files via upload

---
+++ basics of computer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('1 )What is a computer');get_ipython().run_line_magic('pinfo', 'computer')
+Ans:A computer is a machine or device that performs processes, calculations
+    and operations based on instructions provided by a software or hardware program.
+    
+get_ipython().set_next_input('2 )Define types (classifications)of computer?What type of computer do you use');get_ipython().run_line_magic('pinfo', 'use')
+Ans:There are two types of computers based on purpose –
+   1. general 
+   2. special
+    I have a special type of computer.
+    
+get_ipython().set_next_input('3 )Give short history on computers');get_ipython().run_line_magic('pinfo', 'computers')
+Ans:The history of computers starts out about 2000 years ago in Babylonia
+    at("the", "birth", "of", "the", "abacus,", "a", "wooden", "rack", "holding", "two", "horizontal", "wires")
+    with beads strung on them. Blaise Pascal is usually credited for building 
+    the first digital computer in 1642.
+    
+get_ipython().set_next_input('4 )Who is father of computer');get_ipython().run_line_magic('pinfo', 'computer')
+Ans:Charles Babbage is the father of the computer.
+    
+get_ipython().set_next_input('5 )What are the diffrent parts of a computer');get_ipython().run_line_magic('pinfo', 'computer')
+Ans:The 5 different parts of a computer are -
+.A motherboard.
+.A Central Processing Unit (CPU)
+.A Graphics Processing Unit (GPU), also known as a video card.
+.Random Access Memory (RAM), also known as volatile memory.
+.Storage: Solid State Drive (SSD) or Hard Disk Drive (HDD)
+    
+get_ipython().set_next_input('6 )What are the functions of the computer');get_ipython().run_line_magic('pinfo', 'computer')
+Ans:The 4 Functions of a Computer
+    .Data input.   
+    .Data processing.
+    .Information output.
+    .Data and information storage.
+get_ipython().set_next_input('7 )List the components of CPU');get_ipython().run_line_magic('pinfo', 'CPU')
+Ans:The central processing unit (CPU) consists of six main components:
+    .control unit (CU)
+    .arithmetic logic unit (ALU)
+    .registers.
+    .cache.
+    .buses.
+    .clock.
+
